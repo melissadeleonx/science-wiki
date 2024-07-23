ebe827f8ed53b4f0ef0a10716e73d7432b9db00d
melissadeleonx <primavita19@gmail.com>
2024-07-23 08:42:39 +0200

    Ensure dependencies are installed

--- wiki/settings.py
+++ wiki/settings.py
@@ -50,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'wiki.urls'
@@ -123,6 +125,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 # TAILWIND_APP_NAME = 'wikitheme'
