67325d5eca41784f03f9ec11562fa735ee3ce2e2
melissadeleonx <primavita19@gmail.com>
2024-07-24 16:56:51 +0200

    production

--- wiki/settings/production.py
+++ wiki/settings/production.py
@@ -1,12 +1,16 @@
 from .base import *
 
+# Set debug to False for production
 DEBUG = False
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['https://sciencewiki-dba83ffdf24f.herokuapp.com/'])
+
+# Configure allowed hosts
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['sciencewiki-dba83ffdf24f.herokuapp.com'])
 
 # Security settings
-SECURE_HSTS_SECONDS = 31536000
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True  # Apply HSTS to all subdomains
+SECURE_HSTS_PRELOAD = True  # Enable HSTS preload list
+
+SECURE_SSL_REDIRECT = True  # Redirect all HTTP requests to HTTPS
+SESSION_COOKIE_SECURE = True  # Ensure cookies are sent only over HTTPS
+CSRF_COOKIE_SECURE = True  # Ensure CSRF cookies are sent only over HTTPS
