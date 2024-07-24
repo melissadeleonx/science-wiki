f40304c18ec9e913c022201d9acaea69f67e9d0f
melissadeleonx <primavita19@gmail.com>
2024-07-24 16:53:54 +0200

    production

--- wiki/settings/production.py
+++ wiki/settings/production.py
@@ -1,7 +1,7 @@
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['yourdomain.com'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['https://sciencewiki-dba83ffdf24f.herokuapp.com/'])
 
 # Security settings
 SECURE_HSTS_SECONDS = 31536000
